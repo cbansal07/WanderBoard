0d893015d35dbd81d1f7f3325510e6d7b42b656a
cbansal07 <cbansal07@users.noreply.github.com>
2026-09-13 16:19:38 +0530

    fix: switch to Esri map tiles to bypass strict adblockers

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -98,8 +98,8 @@ export function MapView({
       zoomControl={false}
     >
       <TileLayer
-        attribution='&copy; Google Maps'
-        url="https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"
+        attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
+        url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
       />
       <MapController
         center={effectiveCenter}
